3dccbe13b9ca2dfb16b3cf337063a6738c35aadd
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-01-19 18:19:28 +0800

    feat: Support sync for keystone

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -96,6 +96,8 @@ import IconMnemonicWhite from 'ui/assets/walletlogo/IconMnemonic-white.svg';
 import LogoLedgerDark from 'ui/assets/walletlogo/ledgerdark.png';
 import LogoLedgerWhite from 'ui/assets/walletlogo/ledgerwhite.png';
 import IconGridPlus from 'ui/assets/walletlogo/gridplus.png';
+import LogoKeystone from 'ui/assets/walletlogo/keystone.png';
+import LogoKeystoneWithBorder from 'ui/assets/walletlogo/keystone-border.png';
 
 export enum CHAINS_ENUM {
   ETH = 'ETH',
@@ -541,6 +543,7 @@ export const KEYRING_CLASS = {
     LEDGER: 'Ledger Hardware',
     ONEKEY: 'Onekey Hardware',
     GRIDPLUS: 'GridPlus Hardware',
+    KEYSTONE: 'QR Hardware Wallet Device',
   },
   WATCH: 'Watch Address',
   WALLETCONNECT: 'WalletConnect',
@@ -601,6 +604,10 @@ export const HARDWARE_KEYRING_TYPES = {
   GridPlus: {
     type: 'GridPlus Hardware',
     brandName: 'GridPlus',
+  },
+  KeyStone: {
+    type: 'QR Hardware Wallet Device',
+    brandName: 'KeyStone',
   },
 };
 
@@ -735,6 +742,7 @@ export enum BRAND_WALLET_CONNECT_TYPE {
   TrezorConnect = 'TrezorConnect',
   GnosisConnect = 'GnosisConnect',
   GridPlusConnect = 'GridPlusConnect',
+  QRCodeBase = 'QR Hardware Wallet Device',
 }
 
 export const WALLETCONNECT_STATUS_MAP = {
@@ -786,6 +794,7 @@ export enum WALLET_BRAND_TYPES {
   TRUSTWALLET = 'TRUSTWALLET',
   GNOSIS = 'Gnosis',
   GRIDPLUS = 'GRIDPLUS',
+  KEYSTONE = 'Keystone',
 }
 
 export const WALLET_BRAND_CONTENT = {
@@ -852,6 +861,14 @@ export const WALLET_BRAND_CONTENT = {
     icon: IconJade,
     image: LogoJade,
     connectType: BRAND_WALLET_CONNECT_TYPE.WalletConnect,
+  },
+  [WALLET_BRAND_TYPES.KEYSTONE]: {
+    id: 14,
+    name: 'Keystone',
+    brand: WALLET_BRAND_TYPES.KEYSTONE,
+    icon: LogoKeystone,
+    image: LogoKeystoneWithBorder,
+    connectType: BRAND_WALLET_CONNECT_TYPE.QRCodeBase,
   },
   [WALLET_BRAND_TYPES.LEDGER]: {
     id: 4,
